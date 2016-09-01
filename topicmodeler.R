c1c343eb02041f7fde8f2eb303b56a8808144fca
m1kke <koskela.m@gmail.com>
2016-09-01 14:28:50 +0300

    Removes empty documents, cleaning

--- topicmodeler.R
+++ topicmodeler.R
@@ -2,8 +2,6 @@
 library(tm) # for text mining
 library(topicmodels) # for for topic modeling
 library(SnowballC) # for stemming
-library(ggplot2) # for graphs
-library(wordcloud) # for wordclouds
 
 # Create variables
 #dataDir <- "C:/Users/Mikko/Data Analysis Projects/topic-modeling-with-R/data"
@@ -17,8 +15,9 @@ seed <-list(2003,5,63,100001,765)
 nstart <- 5
 best <- TRUE
 k <- 5 # number of topics
+termNum <- 5 # number of terms per topic
 suomiStops <- c("aina", "alla", "ehkä", "eivät", "emme", "en", "enemmän", "ennen", "et", "että", "ette", "hän", "häneen", "hänellä", "hänelle", "häneltä", "hänen", "hänessä", "hänestä", "hänet", "häntä", "he", "heidän", "heidät", "heihin", "heillä", "heille", "heiltä", "heissä", "heistä", "heitä", "hlö", "hlöä", "oikein", "http", "hyvin", "ilman", "itse", "ja", "jälkeen", "johon", "joiden", "joihin", "joiksi", "joilla", "joille", "joilta", "joina", "joissa", "joista", "joita", "joka", "joka", "joksi", "jolla", "jolle", "jolta", "jona", "jonka", "jos", "jossa", "josta", "jota", "jotka", "kai", "kaikki", "kanssa", "kaukana", "keiden", "keihin", "keiksi", "keillä", "keille", "keiltä", "keinä", "keissä", "keistä", "keitä", "keneen", "keneksi", "kenellä", "kenelle", "keneltä", "kenen", "kenenä", "kenessä", "kenestä", "kenet", "kenties", "keskellä", "kesken", "ketä", "ketkä", "ketkä", "koska", "koskaan", "kuin", "kuinka", "kuka", "kun", "kyllä", "lähellä", "läpi", "liian", "lla", "luona", "me", "meidän", "meidät", "meihin", "meillä", "meille", "meiltä", "meissä", "meistä", "meitä", "mihin", "mikä", "miksi", "millä", "mille", "milloin", "milloinkaan", "miltä", "minä", "minkä", "minua", "minulla", "minulle", "minulta", "minun", "minussa", "minusta", "minut", "minuun", "missä", "mistä", "mitä", "miten", "mitkä", "mukaan", "mutta", "muut", "näiden", "näihin", "näiksi", "näillä", "näille", "näiltä", "näinä", "näissä", "näistä", "näitä", "nämä", "ne", "niiden", "niihin", "niiksi", "niillä", "niille", "niiltä", "niin", "niinä", "niissä", "niistä", "niitä", "noiden", "noihin", "noiksi", "noilla", "noille", "noilta", "noin", "noina", "noissa", "noista", "noita", "nopeasti", "nuo", "nyt", "oikea", "oikealla", "ole", "olemme", "olen", "olet", "olette", "oli", "olimme", "olin", "olisi", "olisimme", "olisin", "olisit", "olisitte", "olisivat", "olit", "olitte", "olivat", "olla", "olleet", "ollut", "on", "ovat", "paljon", "poikki", "puh", "saa", "saada", "se", "sekä", "sen", "siellä", "siihen", "siinä", "siitä", "siksi", "sillä", "sille", "siltä", "sinä", "sinua", "sinulla", "sinulle", "sinulta", "sinun", "sinussa", "sinusta", "sinut", "sinuun", "sitä", "ssa", "sta", "suoraan", "tähän", "tai", "takana", "takia", "täksi", "tällä", "tälle", "tältä", "tämä", "tämän", "tänä", "tässä", "tästä", "tätä", "te", "teidän", "teidät", "teihin", "teillä", "teille", "teiltä", "teissä", "teistä", "teitä", "tms", "tuo", "tuoda", "tuohon", "tuoksi", "tuolla", "tuolle", "tuolta", "tuon", "tuona", "tuossa", "tuosta", "tuota", "vaan", "vähän", "vähemmän", "vai", "vain", "vaikka", "vasen", "vasemmalla", "vastan", "vielä", "vieressä", "voi", "voida", "voit", "www", "yhdessä", "yli", "ylös", "yms", "com", "fax", "klo", "myös", "muuta", "viim", "asti", "sis", "koko", "alle", "joskus", "sivu", "paitsi", "sitten", "tule", "auki", "paras", "lue", "lisää", "joko", "ihan", "saat", "ei", "html") # finnish stopwords
-removeSpecials <- function(x) gsub("[^0-9a-zA-ZäÄöÖåÅ ]", "", x)
+removeSpecials <- function(x) gsub("[^0-9a-zA-ZäÄöÖåÅ ]", "", x) # function to remove special chars
 
 # Set working dir to load data
 setwd(dataDir)
@@ -43,19 +42,21 @@ docs <- tm_map(docs, PlainTextDocument) # Turn into plaintext document
 
 # Create DTM
 dtm <- DocumentTermMatrix(docs)
-rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
-dtm.new   <- dtm[rowTotals> 0, ] 
 rownames(dtm) <- filenames
 
+# Remove empty documents from corpus to prevent errors during modeling
+rows.sum = apply(dtm, 1, sum)
+dtm = dtm[rows.sum > 0, ] # tai dtm[rows.sum > 0, ]
+
 # Model topics
-ldaOut <-LDA(dtm.new,k, method="Gibbs", control=list(nstart=nstart, seed = seed, best=best, burnin = burnin, iter = iter, thin=thin))
+ldaOut <-LDA(dtm, k, method="Gibbs", control=list(nstart=nstart, seed = seed, best=best, burnin = burnin, iter = iter, thin=thin))
 
 # Change workdir to results folder
 setwd(resultsDir)
 
 ldaOut.topics <- as.matrix(topics(ldaOut)) #could be tuned
 
-ldaOut.terms <- as.matrix(terms(ldaOut, 5))
+ldaOut.terms <- as.matrix(terms(ldaOut, termNum))
 
 topicProbabilities <- as.data.frame(ldaOut@gamma)
 
